e0571430cdb15dba2c2d7a4792d071ffaf37bb8a
hunterliy <hunterliy10@gmail.com>
2016-12-27 23:16:16 +0800

    实现MVC框架

--- app/src/main/java/com/hunterliy/hunternews/utils/HttpHelper.java
+++ app/src/main/java/com/hunterliy/hunternews/utils/HttpHelper.java
@@ -1,5 +1,40 @@
 package com.hunterliy.hunternews.utils;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
 public class HttpHelper {
+
+    private static final int TIME_OUT_LIMIT = 30;
+    private OkHttpClient mOkHttpClient;
+
+    private HttpHelper(){
+        mOkHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(TIME_OUT_LIMIT, TimeUnit.SECONDS)
+                .writeTimeout(TIME_OUT_LIMIT, TimeUnit.SECONDS)
+                .readTimeout(TIME_OUT_LIMIT, TimeUnit.SECONDS)
+                .build();
+    }
+    private static final HttpHelper getInstance(){
+        return LazyHolder.INSTANCE;
+    }
+
+    private static final class LazyHolder{
+        private static final HttpHelper INSTANCE= new HttpHelper();
+    }
+
+    public Retrofit getRetrofit(String url){
+        return new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+    }
 
 }
